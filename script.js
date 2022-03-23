2e510bf18101cb8be7992cfdc0f2136a4538b5da
Manpreet Singh Khurana <mskhurana728@gmail.com>
2022-03-23 13:12:18 -0400

    done with basic functions add,sub,multibly,divide

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+
+    return (num1 + num2);
+
+};
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+};
+
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+
+}
